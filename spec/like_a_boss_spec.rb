f5d5b8b2c711e24c8eb60ac445e1a9ca92d98027
Dane Harrigan <dane.harrigan@gmail.com>
2011-01-08 13:11:51 -0500

    added more specs to show you can do anything like a boss

--- spec/like_a_boss_spec.rb
+++ spec/like_a_boss_spec.rb
@@ -1,7 +1,16 @@
 require 'spec_helper'
 
 describe LikeABoss do
-  it 'should work like a boss' do
-    1.should == 1.like_a_boss
+  it 'can add like a boss' do
+    (1 + 2).should == 3.like_a_boss
+  end
+
+  it 'can be nil like a boss' do
+    nil.should be_nil.like_a_boss
+  end
+
+  it 'can assign values like a boss' do
+    value = 'Foo'.like_a_boss
+    value.should == 'Foo'
   end
 end
